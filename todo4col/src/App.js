0128891c52b209d7cf85274e7a23ee86aba992e1
bdurb <brian@rpmgreenville.com>
2018-01-17 09:43:09 -0500

    set up state for app

--- todo4col/src/App.js
+++ todo4col/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Column from './Column';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    const savedState = JSON.parse(localStorage.getItem('state'));
+    this.state = savedState || {
+      columns: [
+        {
+          name: 'Back Log',
+          tasks: [],
+        },
+        {
+          name: 'To Do',
+          tasks: [],
+        },
+        {
+          name: 'In Progress',
+          tasks: [],
+        },
+      ],
+    };
   }
 }
 
