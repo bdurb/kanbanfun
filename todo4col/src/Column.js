74d9d7669931f140d8b4a1c9611fe546f993c376
bdurb <brian@rpmgreenville.com>
2018-01-24 14:52:11 -0500

    updated column.js folder

---
+++ todo4col/src/Column.js
@@ -0,0 +1,14 @@
+import Task from './Task';
+import NewTask from './NewTask';
+
+export default (props) => {
+  return(
+    <ul>
+      <h3>{props.data.name}</h3>
+      {props.data.tasks.map((row, taskIndex) => {
+        return <Task key={taskIndex} text={row.text} taskIndex={taskIndex} columnIndex={props.columnIndex} shiftTask={props.shiftTask} />;
+      })}
+      <NewTask columnIndex={props.columnIndex} addTask={props.addTask} />
+    </ul>
+  );
+};
