e6b0422cabd7bbca63bfb7c6d6bf60976e9bf1aa
bdurb <brian@rpmgreenville.com>
2018-01-24 15:26:33 -0500

    updated NewTask

---
+++ todo4col/src/NewTask.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class NewTask extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addTask(this.props.columnIndex, this.state.input);
+    this.setState({ input: ''});
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          value={this.state.input}
+          onChange{(e) => this.setState({ input: e.target.value })} />
+      </form>
+    );
+  }
+}
